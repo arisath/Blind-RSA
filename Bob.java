df89da040028ff2c47da1fd49817cfa3ab7b8113
arisath <arist.athanas@gmail.com>
2015-12-20 04:55:03 +0200

    Update Bob.java

--- Bob.java
+++ Bob.java
@@ -1,6 +1,5 @@
 import org.apache.commons.codec.binary.Base64;
 import org.apache.commons.codec.digest.DigestUtils;
-
 import java.math.BigInteger;
 import java.security.SecureRandom;
 import java.security.interfaces.RSAPublicKey;
@@ -15,7 +14,14 @@ public class Bob
 
     static BigInteger m;
 
-    public static BigInteger calculateMu(RSAPublicKey alicePublicKey)
+    /**
+     * Calculates and returns the mu
+     * Bob uses ALice's public key and a random value r, such that r is relatively prime to N
+     * to compute the blinding factor r^e mod N. Bob then computes the blinded message mu = H(msg) * r^e mod N
+     * It is important that r is a random number so that mu does not leak any information about the actual message
+     * @return the blinded messahe mu
+     */
+    public static BigInteger calculateMu()
     {
         try
         {
@@ -29,8 +35,8 @@ public class Bob
 
             BigInteger N = BlindRsa.N; // get modulus 'N' of the key pair
 
-            // Generate a random number that is in Z*n such that is >1 and therefore r is invertible in Z*n
-            SecureRandom random = SecureRandom.getInstance("SHA1PRNG", "SUN"); //get instance of SecureRandom, we will use it to produce the random number r
+            // Generate a random number so that it belongs to Z*n and is >1 and therefore r is invertible in Z*n
+            SecureRandom random = SecureRandom.getInstance("SHA1PRNG", "SUN");
 
             byte[] randomBytes = new byte[10]; //create byte array to store the r
 
@@ -50,11 +56,12 @@ public class Bob
             while (!gcd.equals(one) || r.compareTo(BlindRsa.N) >= 0 || r.compareTo(one) <= 0); //repeat until getting an r that satisfies all the conditions and belongs to Z*n and >1
 
             //now that we got an r that satisfies the restrictions described we can proceed with calculation of mu
+
             BigInteger mu = ((r.modPow(e, N)).multiply(m)).mod(N); //Bob computes mu = H(msg) * r^e mod N
 
-            return mu; //returned calculated mu
+            return mu;
 
-        }
+        } 
         catch (Exception e)
         {
             e.printStackTrace();
@@ -64,7 +71,8 @@ public class Bob
 
     /**
      * Calculate signature over mu'
-     *
+     * Bob receives the signature over the blinded message that he sent to Alice
+     * and removes the blinding factor to compute the signature over his actual message
      * @param muprime
      * @return signature
      */
@@ -75,7 +83,7 @@ public class Bob
 
             BigInteger N = BlindRsa.N; //get modulus of the key pair
 
-            BigInteger s = r.modInverse(N).multiply(muprime).mod(N); //Bob computes sig = mu'*r^-1 mod N, inverse of r mod N multiplied with muprime mod N
+            BigInteger s = r.modInverse(N).multiply(muprime).mod(N); //Bob computes sig = mu'*r^-1 mod N, inverse of r mod N multiplied with muprime mod N, to remove the blinding factor
 
             byte[] bytes = new Base64().encode(s.toByteArray()); //encode with Base64 encoding to be able to read all the symbols
 
@@ -85,29 +93,26 @@ public class Bob
 
             System.out.println(signature);
 
-            return signature; //return the signature
+            return signature; 
         }
         catch (Exception e)
         {
-            System.out.println(e);
-
+            e.printStackTrace();
             return null;
         }
     }
 
     /**
      * Checks if the signature received from Alice, is a valid signature for the message given, this can be easily computed because(m^d)^e modN = m
-     *
      * @param signature
      */
     public static void verify(String signature)
     {
         try
         {
-            //we need to make a BigInteger based on the signature to be able to do our computations
             byte[] bytes = signature.getBytes(); //create a byte array extracting the bytes from the signature
 
-            byte[] decodedBytes = new Base64().decode(bytes); // decode the bytes with Base64 decoding, remember we encoded we base64 earlier
+            byte[] decodedBytes = new Base64().decode(bytes); // decode the bytes with Base64 decoding (remember we encoded with base64 earlier)
 
             BigInteger sig = new BigInteger(decodedBytes); // create the BigInteger object based on the bytes of the signature
 
@@ -124,16 +129,15 @@ public class Bob
             if (signedMessage.equals(initialMessage)) //compare the two Strings, if they are equal the signature we got is a valid
             {
                 System.out.println("Verification of signature completed successfully"); //print message for successful verification of the signature
-            }
+            } 
             else
             {
                 System.out.println("Verification of signature failed"); // print message for unsuccessful verification of the signature
             }
-
-        }
+        } 
         catch (Exception e)
         {
-            System.out.println(e);
+            e.printStackTrace();
         }
     }
 

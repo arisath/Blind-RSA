671cf1811d18daaac082a2ff40557aaa8e923918
arisath <arist.athanas@gmail.com>
2015-12-20 04:20:24 +0200

    Update Alice.java

--- Alice.java
+++ Alice.java
@@ -8,75 +8,79 @@ import java.security.spec.RSAKeyGenParameterSpec;
  */
 public class Alice
 {
-	/**
-	 * Produces an RSA keypair (N,e,d)
-	 * N: Modulus, e: Public exponent, d: Private exponent
-	 * The public exponent value is set to 3 and the keylength to 2048
-	 * @return RSA keypair
-	 */
-	public static KeyPair produceKeyPair()
-	{
-		try
-		{
-			KeyPairGenerator rsaKeyPairGenerator = KeyPairGenerator.getInstance("RSA");  //get rsa key generator
+    /**
+     * Produces and returns an RSA keypair (N,e,d)
+     * N: Modulus, e: Public exponent, d: Private exponent
+     * The public exponent value is set to 3 and the keylength to 2048
+     * @return RSA keypair
+     */
+    public static KeyPair produceKeyPair()
+    {
+        try
+        {
+            KeyPairGenerator rsaKeyPairGenerator = KeyPairGenerator.getInstance("RSA");  //get rsa key generator
 
-			RSAKeyGenParameterSpec spec = new RSAKeyGenParameterSpec(2048, BigInteger.valueOf(3)); //set the parameters for they key, key length=2048, public exponent=3
+            RSAKeyGenParameterSpec spec = new RSAKeyGenParameterSpec(2048, BigInteger.valueOf(3)); //set the parameters for they key, key length=2048, public exponent=3
 
-			rsaKeyPairGenerator.initialize(spec); //initialise generator with the above parameters
+            rsaKeyPairGenerator.initialize(spec); //initialise generator with the above parameters
 
-			KeyPair keyPair = rsaKeyPairGenerator.generateKeyPair(); //generate the key pair, N:modulus, d:private exponent
+            KeyPair keyPair = rsaKeyPairGenerator.generateKeyPair(); //generate the key pair, N:modulus, d:private exponent
 
-			return (keyPair);  //return the key pair produced (N,e,d)
+            return (keyPair);  //return the key pair produced (N,e,d)
 
-		}
-		catch (Exception e)
-		{
-			System.out.println(e);
+        } 
+        catch (Exception e)
+        {
+            e.printStackTrace();
+            return null;
+        }
+    }
 
-			return null;
-		}
-	}
+    /**
+     * Calculate mu' using the Chinese Remainder Theorem for optimization
+     * Thanks to the isomorphism property f(x+y)=f(x)+f(y) we can split the mu^d modN in two:
+     * one mode p , one mode q, and then we can combine the results to calculate muprime
+     * @param mu
+     * @return mu'
+     */
+    public static BigInteger calculateMuPrimeWithChineseRemainderTheorem(BigInteger mu)
+    {
+        try
+        {
+            BigInteger N = BlindRsa.N; //get modulus N
 
-	/**
-	 * Calculate mu' using the Chinese Remainder Theorem for optimization
-	 *
-	 * @param mu
-	 * @return mu'
-	 */
-	public static BigInteger calculateMuPrimeWithChineseRemainderTheorem(BigInteger mu)
-	{
-		try
-		{
-			BigInteger N = BlindRsa.N; //get modulus N
+            BigInteger P = BlindRsa.alicePrivate.getPrimeP(); //get the prime number p used to produce the key pair
 
-			BigInteger P = BlindRsa.alicePrivate.getPrimeP(); //get the prime number p used to produce the key pair
+            BigInteger Q = BlindRsa.alicePrivate.getPrimeQ(); //get the prime number q used to produce the key pair
 
-			BigInteger Q = BlindRsa.alicePrivate.getPrimeQ(); //get the prime number q used to produce the key pair
+            //We split the mu^d modN in two , one mode p , one mode q
 
-			//Chinese remainder theorem: we split the mu^d modN in two , one mode p , one mode q then we combine the results to calculate muprime, we can do this because of the isomorphism property f(x+y)=f(x)+f(y)
-			BigInteger PinverseModQ = P.modInverse(Q); //calculate p inverse modulo q
+            BigInteger PinverseModQ = P.modInverse(Q); //calculate p inverse modulo q
 
-			BigInteger QinverseModP = Q.modInverse(P); //calculate q inverse modulo p
+            BigInteger QinverseModP = Q.modInverse(P); //calculate q inverse modulo p
 
-			BigInteger d = BlindRsa.alicePrivate.getPrivateExponent(); //get private exponent d
+            BigInteger d = BlindRsa.alicePrivate.getPrivateExponent(); //get private exponent d
 
-			//split the message mu in to messages m1, m2 one mod p, one mod q
-			BigInteger m1 = mu.modPow(d, N).mod(P); //calculate m1=(mu^d modN)modP
+            //We split the message mu in to messages m1, m2 one mod p, one mod q
 
-			BigInteger m2 = mu.modPow(d, N).mod(Q); //calculate m2=(mu^d modN)modQ
+            BigInteger m1 = mu.modPow(d, N).mod(P); //calculate m1=(mu^d modN)modP
 
-			//combine the calculated m1 and m2 in order to calculate muprime
-			BigInteger muprime = ((m1.multiply(Q).multiply(QinverseModP)).add(m2.multiply(P).multiply(PinverseModQ))).mod(N); //calculate muprime: (m1*Q*QinverseModP + m2*P*PinverseModQ) mod N where N =P*Q
+            BigInteger m2 = mu.modPow(d, N).mod(Q); //calculate m2=(mu^d modN)modQ
 
-			return muprime; //return muprime
+            //We combine the calculated m1 and m2 in order to calculate muprime
+            //We calculate muprime: (m1*Q*QinverseModP + m2*P*PinverseModQ) mod N where N =P*Q
+            
+            BigInteger muprime = ((m1.multiply(Q).multiply(QinverseModP)).add(m2.multiply(P).multiply(PinverseModQ))).mod(N);
 
-		}
-		catch (Exception e)
-		{
-			System.out.println(e);
-		}
-		return null;
-	}
+            return muprime;
+
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+        return null;
+    }
 
 }
 
